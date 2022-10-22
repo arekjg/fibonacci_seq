8e05b21b3107068078460037842697fe615d3ed5
arekjg <arekjg@gmail.com>
2022-10-22 21:56:45 +0200

    added commenst, changed list name, added case when user inserts 1

--- fibonacci_seq.cpp
+++ fibonacci_seq.cpp
@@ -5,30 +5,41 @@
 int main()
 {
     int num;
-    std::list<int> l = { 0, 1 };
+    std::list<int> fib = { 0, 1 };
     
-
     std::cout << "How many numbers of Fibonacci Sequence do you want to print? ";
     std::cin >> num;
 
+    // Declare variables
+    // prev1 - last number in sequence
+    // prev2 - second to last number in sequence
+    // next - sum of prev1 and prev2, next number in sequence
     int prev1;
     int prev2;
     int next;
 
+    // Calculate next number and insert it to the list
     for (int i = 0; i < num - 2; i++)
     {
-        std::list<int>::iterator it = l.end();
+        std::list<int>::iterator it = fib.end();
         it--;
         prev1 = *(it);
         it--;
         prev2 = *(it);
         next = prev1 + prev2;
-        l.insert(l.end(), next);
-        std::cout << prev1 << "   " << prev2 << "   " << next << "\n";
+        fib.insert(fib.end(), next);
     }
 
-    for (int n : l)
+    // Print the sequence
+    if (num == 1)
     {
-        std::cout << n << ", ";
+        std::cout << "0";
+    }
+    else
+    {
+        for (int n : fib)
+        {
+            std::cout << n << "\n";
+        }
     }
 }
